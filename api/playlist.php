7b5a1ddd343fc9640ccc3ba62efd20d3bbbeb79c
harshuma28 <harshitrathi28@gmail.com>
2024-09-09 11:00:23 +0530

    Update playlist.php

--- api/playlist.php
+++ api/playlist.php
@@ -29,8 +29,8 @@ foreach ($channels as $channel) {
     if ($dashUrl === null) {
         continue;
     }
-    $extension = pathinfo(parse_url($dashUrl, PHP_URL_PATH), PATHINFO_EXTENSION);
-    $playlistUrl = "https://$serverAddress/{$id}.$extension|X-Forwarded-For=59.178.72.184";
+    //$extension = pathinfo(parse_url($dashUrl, PHP_URL_PATH), PATHINFO_EXTENSION);
+    //$playlistUrl = "https://$serverAddress/{$id}.$extension|X-Forwarded-For=59.178.72.184";
     $m3u8PlaylistFile .= "#EXTINF:-1 tvg-id=\"{$id}\" tvg-logo=\"https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/{$channel['channel_logo']}\" group-title=\"{$channel['channel_genre'][0]}\",{$channel['channel_name']}\n";
     $m3u8PlaylistFile .= "#KODIPROP:inputstream.adaptive.license_type=clearkey\n";
     $m3u8PlaylistFile .= "#KODIPROP:inputstream.adaptive.license_key=https://tpck.drmlive-01.workers.dev/?id={$id}\n";
