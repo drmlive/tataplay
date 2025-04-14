e5dd88d8da69c216fe2fde446d91fe0909b1ed6c
toor <ttoor835@gmail.com>
2025-04-15 03:11:52 +0530

    api update

--- app/functions.php
+++ app/functions.php
@@ -5,11 +5,12 @@ $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
 $loginFilePath = 'app/data/login.json';
 $cachePath = 'app/data/cache_urls.json';
 $origin_json = 'app/origin.json';
+$aesKey = 'aesEncryptionKey';
 $ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36';
-$content_api = 'https://tb.tapi.videoready.tv/content-detail/api/partner/cdn/player/details/fucktp/' . $id;
-$decryptApiUrl = 'https://tp-aes-decrypt.vercel.app';
-$origin_api = 'https://tp-aes-decrypt.vercel.app/origin';
-$stb_only = 'https://tp-aes-decrypt.vercel.app/stb_only';
+$content_api = 'https://tb.tapi.videoready.tv/content-detail/api/partner/cdn/player/details/chotiluli/' . $id;
+$origin_api = 'https://tp.drmlive-01.workers.dev/origin';
+$stb_only = 'https://tp.drmlive-01.workers.dev/stb_only';
+
 function extractPsshFromManifest(string $content, string $baseUrl, string $userAgent): ?array {
     if (($xml = @simplexml_load_string($content)) === false) return null;
     foreach ($xml->Period->AdaptationSet as $set) {
@@ -56,6 +57,14 @@ function extractKidandpssh($hexContent) {
 function isApacheCompatible(): bool {
     $software = $_SERVER['SERVER_SOFTWARE'] ?? '';
     return stripos($software, 'Apache') !== false || stripos($software, 'LiteSpeed') !== false;
+}
+
+function decryptUrl($encryptedUrl, $aesKey) {
+    $cleanEncrypted = preg_replace('/#.*$/', '', $encryptedUrl);
+    $decoded = base64_decode($cleanEncrypted);
+    $decryptedUrl = openssl_decrypt($decoded, 'aes-128-ecb', $aesKey, OPENSSL_RAW_DATA);
+    if ($decryptedUrl === false) {return false;}
+    return $decryptedUrl;
 }
 
 ?>
