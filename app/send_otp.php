f63ab786082b6e72d1cc57d153f5f820e23ae6e7
toor <ttoor835@gmail.com>
2026-03-16 02:56:28 +0530

    faaahhhhhhhh         'Expect:'

--- app/send_otp.php
+++ app/send_otp.php
@@ -27,6 +27,7 @@ if (!file_exists($credFile)) {
         "deviceid: $deviceId",
         'origin: https://www.tataplaybinge.com',
         'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+        'Expect:'
     ]);
 
     $guestResponse = curl_exec($ch);
